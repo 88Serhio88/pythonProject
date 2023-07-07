603ab0dcea4c4deac105cb423c47155799ff2bf3
88Serhio88 <129792566+88Serhio88@users.noreply.github.com>
2023-07-07 23:32:15 +0300

    Update test.py

--- learn/test.py
+++ learn/test.py
@@ -1,4 +1,22 @@
-# for i in range(1, 6):
-#     for b in range(i, 6):
-#         print(b, end="")
-#     print("")
+# Standart - smallest, sell Amstrad nats.
+
+from math import ceil
+
+def is_palindrome(text):
+    text = ''.join(e for e in text if e.isalnum())
+    text = text.lower()
+    x = text[:len(text) // 2]
+    y = text[len(text) // 2:]
+    x_1 = text[:ceil(len(text) / 2)]
+    y_1 = text[ceil(len(text) / 2):]
+    if len(text) % 2 == 0:
+        print("True" if x == y[::-1] else "False")
+    elif len(text) % 2 != 0:
+        print("True" if x_1 == y_1[::-1] else "False")
+
+
+# считываем данные
+txt = input()
+
+# вызываем функцию
+print(is_palindrome(txt))
